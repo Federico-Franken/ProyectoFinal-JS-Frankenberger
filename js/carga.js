927d27b173f74173266745c77ca0bee38eab5317
Federico Agustin Frankenberger <fafrankenberger@gmail.com>
2023-12-13 03:33:08 -0300

    Filtros - Todos

--- js/carga.js
+++ js/carga.js
@@ -1,61 +1,17 @@
 
+document.addEventListener("DOMContentLoaded", function(){
+
 const cargaCliente = document.querySelector("#cargaCliente")
+
 const cargaTurnos = document.getElementById("cargaTurnos")
 
-cargaCliente.addEventListener("submit", (e)=>{
-    e.preventDefault()
-    const dia = document.querySelector("#dia").value
-    const nombre = document.querySelector("#nombre").value
-    const email = document.querySelector("#email").value
-    const telefono = document.querySelector("#telefono").value
+const botones = ["TODOS", "LUNES", "MARTES", "MIÉRCOLES", "JUEVES", "VIERNES", "SÁBADO"]
 
-    const Clientes = JSON.parse(localStorage.getItem("cliente")) || []
-    const isClienteRegistered = Clientes.find (client => client.email === email)
-    if (isClienteRegistered){
-        return alert ("El cliente ya cuenta con un turno asignado!")
-    }
-
-    Clientes.push({dia: dia, nombre: nombre, email: email, telefono: telefono})
-    localStorage.setItem("cliente", JSON.stringify(Clientes))
-    
-   
-   
-})
-
-cargarTurnos()
-cargaCliente.addEventListener("submit", cargarTurnos)
-
-/* function cargarTurnos () {
+function cargarTurnos (turnosfiltrados) {
     cargarTurnos.innerHTML = ""
     limpiarTurnos()
-    const Clientes2 = JSON.parse(localStorage.getItem("cliente")) ||[]
-    Clientes2.forEach((elemento) => {
-
-    let content = document.createElement("div")
-    content.className = "turnos-styles col-3"
-    content.innerHTML = `
-    <p>Día: ${elemento.dia}</p>
-    <p>Nombre: ${elemento.nombre}</p>
-    <p>Email: ${elemento.email}</p>
-    <p>Teléfono: ${elemento.telefono}</p>
-    <span class="delete-turno"> ❌ </span>
-
-    `
-    cargaTurnos.append(content);
-
-    let eliminar = content.querySelector(".delete-turno")
-    eliminar.addEventListener("click", () => {
-        borrarTurnos(elemento.email)
-    })
-    
-});
-} */
-function cargarTurnos () {
-    cargarTurnos.innerHTML = ""
-    limpiarTurnos()
-    const Clientes2 = JSON.parse(localStorage.getItem("cliente")) ||[]
-    Clientes2.forEach((elemento) => {
-
+    const turnos = turnosfiltrados || obternerTurnos()
+    turnos.forEach((elemento) => {
     let fila = document.createElement("tr")
     fila.className="tr-dom"
     fila.innerHTML = `
@@ -74,11 +30,12 @@ function cargarTurnos () {
     let eliminar = fila.querySelector(".delete-turno")
     eliminar.addEventListener("click", () => {
         borrarTurnos(elemento.email)
+        
     })
+    
     
 });
 }
-
 
 function limpiarTurnos () {
     while (cargaTurnos.firstChild){
@@ -87,16 +44,74 @@ function limpiarTurnos () {
 }
 
 function borrarTurnos (email) {
-    const borro = JSON.parse(localStorage.getItem("cliente")) || []
+    const borro = obternerTurnos()
     const indexTurno = borro.findIndex (x => x.email === email)
     if (indexTurno !== -1){
         borro.splice(indexTurno,1)
         localStorage.setItem("cliente", JSON.stringify(borro))
 
-        cargarTurnos();
+        cargarTurnos(borro);
     }
     
 }
+
+function cargarBotones () {
+    
+    const contenedorFiltros = document.querySelector("#nav-filter")
+
+    botones.forEach((dia)=>{
+        const filtro = document.createElement("li")
+        filtro.innerHTML= `
+        <a class="nav-link" href="#">${dia}</a>
+        `
+        filtro.addEventListener("click",()=>{
+
+            const turnosfiltrados = filtrarTurnos(dia)
+            
+            cargarTurnos(turnosfiltrados)
+            
+        })
+        contenedorFiltros.appendChild(filtro)
+    })
+}
+
+function filtrarTurnos (filtrar){
+    const turnosFiltrados = obternerTurnos().filter((turno)=>turno.dia === filtrar)
+    return turnosFiltrados
+}
+
+function obternerTurnos (){
+   return JSON.parse(localStorage.getItem("cliente")) || []
+}
+
+cargarBotones()
+
+cargaCliente.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    const dia = document.querySelector("#dia").value
+    const nombre = document.querySelector("#nombre").value
+    const email = document.querySelector("#email").value
+    const telefono = document.querySelector("#telefono").value
+
+    const Clientes = JSON.parse(localStorage.getItem("cliente")) || []
+    const isClienteRegistered = Clientes.find (client => client.email === email)
+    if (isClienteRegistered){
+        return alert ("El cliente ya cuenta con un turno asignado!")
+    }
+
+    Clientes.push({dia: dia, nombre: nombre, email: email, telefono: telefono})
+    localStorage.setItem("cliente", JSON.stringify(Clientes))
+    
+    cargarTurnos(Clientes)
+    
+})
+
+})
+    
+
+
+
+
 
 
 
